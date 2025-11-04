4099b1cc3cfeee3f1b3d339789d96ac1962ccc2f
Muttu Naik <muttunaik5096@gmail.com>
2025-11-04 19:55:20 +0530

    Create src_config.sv

---
+++ source_agent_top/src_config.sv
@@ -0,0 +1,14 @@
+class src_config extends uvm_object;
+
+        `uvm_object_utils(src_config)
+
+        virtual router_if       vif;
+        uvm_active_passive_enum is_active = UVM_ACTIVE;
+
+        extern function new(string name = "src_config");
+
+endclass
+
+function src_config :: new (string name);
+        super.new(name);
+endfunction:new
