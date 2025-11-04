de0aef2b26247fb5c54daac31e1477456abbfaf9
Muttu Naik <muttunaik5096@gmail.com>
2025-11-04 22:34:00 +0530

    Create dst_config.sv

---
+++ destination_agent_top/dst_config.sv
@@ -0,0 +1,15 @@
+class dst_config extends uvm_object;
+
+        `uvm_object_utils(dst_config)
+
+        virtual router_if vif;
+        uvm_active_passive_enum is_active = UVM_ACTIVE;
+
+        extern function new(string name = "dst_config");
+
+endclass
+
+
+function dst_config :: new(string name);
+        super.new(name);
+endfunction:new
